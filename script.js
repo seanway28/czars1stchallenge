31d8130a62c731ebe5abf4a554e9fc98c4ad4340
seanway28 <seanway28@gmail.com>
2021-05-10 18:38:43 -0400

    capture user input

--- script.js
+++ script.js
@@ -14,60 +14,7 @@ function writePassword() {
 }
 
 function generatePassword () {
-  var characterLength = prompt("How many characters would you like? (8 to 128 characters pls)")
-
-  while(characterLength < 8 || characterLength > 128) {
-    characterLength = prompt("Yo 8 to 128 bro, try again")
-  }
-
-  var lowercaseCheck = prompt("Do you want lower case characters?")
-
-  if (lowercaseCheck==="yes") {
-    lowercaseCheck = true
-  } else {
-    lowercaseCheck = false
-  }
-
-  var UppercaseCheck = prompt("Do you want upper case characters?")
-
-  if (UppercaseCheck==="yes") {
-    UppercaseCheck = true
-  } else {
-    UppercaseCheck = false
-  }
-
-  
-  var numericCheck = prompt("Do you want numbers?")
-  
-  if (numericCheck==="yes") {
-    numericCheck = true
-  } else {
-    numericCheck = false
-  }
-  
-  var specialCharCheck = prompt("Do you want special characters?")
-  const specialChars = `~\`!#$&@+=-*[]';,\{}|\\":<>?`;
-
-  if (specialCharCheck==="yes") {
-    specialCharCheck = true
-  } else {
-    specialCharCheck = false
-  }
-
-  console.log(lowercaseCheck, UppercaseCheck, numericCheck, specialCharCheck);
-
-  var password = '';
-
-  for (i = 0; i <= Number(characterLength); i++) {
-    if (lowercaseCheck) {
-      var lowerString = Math.random().toString(36).slice(2).replace(/^[0-9]g/)
-      console.log(lowerString)
-    }
-  }
-
-
-  console.log(lowercaseCheck)
-  return "Sean"
+ 
 }
 
 // Add event listener to generate button
