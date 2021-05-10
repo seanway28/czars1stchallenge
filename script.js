55bb84cce18bf771a8d132195f5ae017ce45839f
seanway28 <seanway28@gmail.com>
2021-05-10 18:45:13 -0400

    generate password

--- script.js
+++ script.js
@@ -57,7 +57,48 @@ function generatePassword () {
     if (specialChar === 'yes') {
       charTypes.push('special');
     }
+
+    let password = '';
+    console.log('Lenght: ', Number(passwordLength));
+
+    for (i = 0; password.length < Number(passwordLength); i++) {
+      for (j = 0; j < charTypes.length; j++) {
+        if (charTypes[j] === 'lowercase') {
+          // Generate a string of lowercase alphanumeric characters
+          let lowerString = Math.random().toString(36).slice(2);
+          lowerString = lowerString.replace(/\d+/g, '');
+          password += lowerString[0];
+        }
+
+        if (charTypes[j] === 'uppercase') {
+          let upperString = '';
+          // Generate a string of uppercase alphanumeric characters
+          upperString = Math.random().toString(36).toUpperCase().slice(2);
+          upperString = upperString.replace(/\d+/g, '');
+          password += upperString[0];
+        }
+
+        if (charTypes[j] === 'numeric') {
+          // Generate a random number
+          let num = Math.floor(Math.random() * 10);
+          num = num.toString();
+          password += num;
+        }
+
+        if (charTypes[j] === 'special') {
+          const specialChars = `~\`!#$%^&*+=-[]';,/{}|\\":<>?`;
+          let specialChar = '';
+          // Select a random special character from the list
+          specialChar =
+            specialChars[Math.floor(Math.random() * specialChars.length)];
+          // Add the special character to the current password
+          password += specialChar;
+        }
+      }
+    }
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
