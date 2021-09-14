7b61177b30cfe5525b85343dccc67a3570dc2d71
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-14 07:57:37 -0400

    client / src / index

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import jobSearch from './jobSearch';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+<React.StrictMode>
+    <App />,
+    </React.StrictMode>,
+  document.getElementById('root'));
+
+  export default function Indeed(url = 'http://api.indeed.com/ads', publisherId) {
+  if (!publisherId) {
+    throw Error('An Indeed publisher id is required');
+  } else {
+    return {
+      jobSearch: () => new jobSearch(url, publisherId)
+    };
+  }
+}
+
+
+reportWebVitals();
